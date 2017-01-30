08b083781859caed5d2cf3131564ad4572453830
West <arsiddde@gmail.com>
2017-01-30 23:11:04 +0300

    isolate scene class from UI controller

---
+++ public/js/learning/scene.js
@@ -0,0 +1,67 @@
+class Scene {
+  constructor($modelName, $stateImg, $stateParams, $event, $response, $tools) {
+    this.$modelName = $modelName;
+    this.$stateImg = $stateImg;
+    this.$stateParams = $stateParams;
+    this.$event = $event;
+    this.$response = $response;
+    this.$tools = $tools;
+  }
+
+  /** Set actual node content */
+  setModelName(name) {
+    this.$modelName.textContent = name;
+    return this;
+  }
+
+  /** Create input elements in 'tools' block */
+  initTools(tools) {
+    const html = tools.map(tool => {
+      return `<div class='tool'>${this.createTool(tool)}</div>`
+    }).join('');
+
+    this.$tools.innerHTML = html;
+    return this;
+  }
+
+  createTool(tool) {
+    switch (tool.type) {
+      case 'range':
+        return `
+          <label for='${tool.name}'>${tool.name}</label>
+          <div>
+            <span>${tool.min}</span>
+            <input data-id='${tool.id}' name='${tool.name}' type='range' min='${tool.min}' max='${tool.max}'>
+            <span>${tool.max}</span>
+          </div>
+          `
+      case 'switch':
+        return `
+          <label for="${tool.name}">${tool.name}</label>
+          <label class='switch'>
+            <input type='checkbox'>
+            <div class='slider'></div>
+          </label>
+        `
+      default:
+        return 'Unknown tool type';
+    }
+  }
+
+  /** Setting img and parameters of current state to UI */
+  setState({ img, params }) {
+    this.$stateImg.setAttribute('src', img);
+    let html = '';
+    for (let key in params) {
+      html += `
+        <div>
+          <span>${key}:</span>
+          <span>${params[key]}</span>
+        </div>
+      `
+    }
+    this.$stateParams.innerHTML = html;
+    return this;
+  }
+
+}
--- public/js/learning/uiController.js
+++ public/js/learning/uiController.js
@@ -2,15 +2,14 @@
  * Singleton object handling all interface changes
  */
 const UIController = (function() {
-  /**
-   * Scene elements
-   */
-  $modelName = document.querySelector('.model-name');
-  $stateImg = document.querySelector('.state-visual');
-  $stateParams = document.querySelector('.state-params');
-  $event = document.querySelector('.event');
-  $response = document.querySelector('.response');
-  $tools = document.querySelector('.tools');
+  const scene = new Scene(
+    document.querySelector('.model-name'),
+    document.querySelector('.state-visual'),
+    document.querySelector('.state-params'),
+    document.querySelector('.event'),
+    document.querySelector('.response'),
+    document.querySelector('.tools')
+  )
 
   /**
    * Load model with given id
@@ -35,64 +34,6 @@ const UIController = (function() {
   }
 
   /**
-   * Set dinamic parts of the model to UI
-   * State image / state params e.g.
-   */
-  // TODO
-  function setScene(model) {
-    $stateImg.setAttribute('src', model.currentState.img);
-    const { params } = model.currentState;
-    let html = '';
-    for (param in params) {
-      html += `
-        <div>
-          <span>${param}:</span>
-          <span>${params[param]}</span>
-        </div>
-      `
-    }
-    $stateParams.innerHTML = html;
-  }
-
-  /**
-   * Set tools from received model to UI
-   */
-  function initTools(tools) {
-    const html = tools.map(tool => {
-      return `<div class='tool'>${createTool(tool)}</div>`
-    }).join('');
-
-    $tools.innerHTML = html;
-  }
-
-  /**
-   * Create html for single tool
-   */
-  function createTool(tool) {
-    switch (tool.type) {
-      case 'range':
-        return `
-          <label for='${tool.name}'>${tool.name}</label>
-          <div>
-            <span>${tool.min}</span>
-            <input data-id='${tool.id}' name='${tool.name}' type='range' min='${tool.min}' max='${tool.max}'>
-            <span>${tool.max}</span>
-          </div>
-          `
-      case 'switch':
-        return `
-          <label for="${tool.name}">${tool.name}</label>
-          <label class='switch'>
-            <input type='checkbox'>
-            <div class='slider'></div>
-          </label>
-        `
-      default:
-        return 'Unknown tool type';
-    }
-  }
-
-  /**
    * UI button's handlers
    */
   $loadButton = document.querySelector('#load-model-btn');
@@ -102,13 +43,14 @@ const UIController = (function() {
     loadModel(modelId)
       .then(model => {
         /** initializing static parts of UI */
-        $modelName.textContent = model.name;
-        initTools(model.tools);
+        scene.setModelName(model.name).initTools(model.tools);
+
+        /** creating model from received data */
         currentModel = new Model(model);
-        /** dynamic parts */
-        setScene(currentModel);
+        /** initializing dynamic parts */
+        scene.setState(currentModel.currentState);
       })
-      .catch(err => { console.error(err.message) });
+      .catch(err => { console.error(err) });
   });
 
   $startButton = document.querySelector('#start-btn');
