a608413a5cd1dc257f3dad12ca14aa369c68238e
West <arsiddde@gmail.com>
2017-04-05 02:45:21 +0300

    fixed login redirection bug

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,11 +1,14 @@
 function auth(req, res, next) {
   if (req.session.user) {
     next();
-  } else {
-    // Save requested path to redirect user there after authorization
+  } else if (req.headers && !req.headers.referer) {
+    // Если запрос пришел от пользователя, а не от страницы
+    // Сохранить путь для дальнейшего перенаправления после авторизации
     req.session.requestedPath = req.path;
     console.log('Saved path:', req.path);
     res.redirect('/login');
+  } else {
+    res.sendStatus(404);
   }
   // req.session.user = {
   //   id: 1,
